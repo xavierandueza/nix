e30506bc7f9e54ef2dd8ee1dd573813d7489253a
Xavier Andueza <xavier@lyratechnologies.com.au>
2026-08-28 10:19:33 +1000

    chore: better neru setup

--- programs/neru.nix
+++ programs/neru.nix
@@ -3,6 +3,10 @@
 {
   xdg.configFile."neru/config.toml".text = ''
     [hotkeys]
-    "Primary+Shift+Space" = "grid"
+    "Primary+Shift+Space" = "grid --action left_click"
+    "Primary+Shift+C" = "__disabled__"
+
+    [grid.hotkeys]
+    "Shift+L" = ["action left_click", "idle"]
   '';
 }
