6bed62b46a5617439cd75044de647b7185d31b1f
Xavier Andueza <xavier@lyratechnologies.com.au>
2026-09-15 13:24:03 +1000

    feat: specify herdr and more matt pocock skills

--- programs/skills.nix
+++ programs/skills.nix
@@ -7,7 +7,7 @@
 {
   home.activation.agentSkills = lib.hm.dag.entryAfter [ "linkGeneration" ] ''
     export DISABLE_TELEMETRY=1
-    export PATH="${lib.makeBinPath [ pkgs.nodejs_22 pkgs.git ]}:$PATH"
+    export PATH="${lib.makeBinPath [ pkgs.nodejs_22 pkgs.git pkgs.gh ]}:$PATH"
 
     install_skills() {
       source="$1"
@@ -22,9 +22,10 @@
 
     install_skills xavierandueza/agents '*'
     install_skills xavierandueza/loops '*'
-    install_skills herdrdev/herdr '*'
+    install_skills herdrdev/herdr herdr
     install_skills langfuse/skills '*'
     install_skills microsoft/playwright-cli playwright-cli
+    install_skills readmeio/skills thermo-nuclear-code-quality-review
     install_skills mattpocock/skills \
       codebase-design \
       diagnosing-bugs \
